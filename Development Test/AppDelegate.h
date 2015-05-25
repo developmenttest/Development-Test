12fb9e2669c23ae8049d8fd62e75451b3b9a8777
Development Test <newdevelopmenttest@gmail.com>
2015-05-25 12:46:30 +0530

    Create Project

---
+++ Development Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Development Test
+//
+//  Created by Chintan Dave on 25/05/15.
+//  Copyright (c) 2015 Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Development Test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Development Test
+//
+//  Created by Chintan Dave on 25/05/15.
+//  Copyright (c) 2015 Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
